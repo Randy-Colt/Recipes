335cc1a9240ee29ec19fc7bfeaac2e6a97c7cfc5
Андрей Логвинов <andryu04101999@gmail.com>
2024-11-09 20:09:10 +0300

    flake

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.core.signals import request_finished
 
 
 class RecipesConfig(AppConfig):
@@ -7,4 +8,7 @@ class RecipesConfig(AppConfig):
     verbose_name = 'РЕЦЕПТЫ'
 
     def ready(self):
-        import recipes.signals
+        from recipes import signals
+        request_finished.connect(signals.create_converter)
+        request_finished.connect(signals.auto_delete_file_on_change)
+        request_finished.connect(signals.auto_delete_file_on_delete)
